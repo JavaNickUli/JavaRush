2dd9d12da2eb08a11846e8414cb57229ead25fcb
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:05:38 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0203/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task02.task0203;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 546;
+        int lastDigit = number % (number / 10);
+        System.out.println(lastDigit);
+    }
+}
