e202635f9497b87ec0f63ab753835e962e83e511
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:13:20 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0205/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0205;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int toothCounter = 32;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        System.out.println(toothCounter);
+    }
+}
