26a0548f732fcceb30f36d3eae4bbdd4d5391070
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:01:36 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task02.task0202;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+        int meters = centimeters / 100;
+        System.out.println(meters);
+    }
+}
