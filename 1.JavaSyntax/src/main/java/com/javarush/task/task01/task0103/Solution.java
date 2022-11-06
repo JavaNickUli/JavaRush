9e31f369df56ae9a01acd5a821628b46e2139b60
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 10:39:55 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task01.task0103;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int year = 3126;
+        int age = 8;
+        System.out.println(year - age);
+    }
+}
