e0734d67e9236b1b8798896599dd256cc26cc505
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 08:37:18 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/task01/task0101/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task01.task0101;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Я думаю, быть программистом круто");
+    }
+}
