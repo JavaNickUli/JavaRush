a6fa627d1b929501fb47174a6b78db5c14608503
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:09:52 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0204/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0204;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int six = 6;
+        six++;
+        six++;
+        six++;
+        System.out.println(six);
+    }
+}
