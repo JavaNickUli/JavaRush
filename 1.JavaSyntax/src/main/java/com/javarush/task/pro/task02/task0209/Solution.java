2d7bace7655f1f6fe16411e9d8908f57bab283dd
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:28:47 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0209/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0209;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        int z = 0;
+        String digits = x + y + "" + z;
+        System.out.println(digits);
+    }
+}
