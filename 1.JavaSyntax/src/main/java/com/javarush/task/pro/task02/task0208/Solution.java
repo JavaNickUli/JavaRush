9d44db3b96a2e4d21a0f501904bd46cb809ce594
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:24:57 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0208/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task02.task0208;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptiness = "пустота";
+        String fullness = emptiness + " " + emptiness + " " + emptiness;
+        System.out.println(fullness);
+    }
+}
