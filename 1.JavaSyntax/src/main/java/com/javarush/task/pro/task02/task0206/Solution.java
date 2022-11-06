ce75e17d7968c8bdae5e2fe938bc9a37843dd3ab
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 11:16:53 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Word";
+        String phrase = "Phrase";
+        String line = "Line";
+        String text = "Text";
+    }
+}
