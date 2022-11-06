50b81dcc8d0ced2c3ce9aad4a2a9001d18e04786
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 10:33:54 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task01.task0105;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String city;
+    }
+}
