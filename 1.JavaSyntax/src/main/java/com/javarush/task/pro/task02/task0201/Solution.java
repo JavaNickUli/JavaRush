8fb452b6d89bcf029cdb3425a1b9ba21c3afc98c
JavaNickUli <java.nickuli@mail.ru>
2022-11-06 10:57:35 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0201;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 2;
+        int numericValue = 4;
+        int size = 8;
+        int number = 16;
+    }
+}
