ab3322fcdee76508e0a8ac42593322f9a5debea0
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 17:13:12 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task12.task1216;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public interface CanFly {
+
+        void canFly();
+
+        boolean isFly();
+    }
+}
