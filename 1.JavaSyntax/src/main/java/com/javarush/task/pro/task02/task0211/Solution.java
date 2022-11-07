08e0bd9a5442e817ef2955a42a782beacf1755f3
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 18:13:21 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0211/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0211;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptyString = "";
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
+    }
+}
