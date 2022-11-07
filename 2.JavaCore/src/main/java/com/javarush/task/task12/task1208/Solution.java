3053cda1907c86d7ac39b00dbca153f9a34883dd
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 15:18:21 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1208;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static void print() {
+    }
+
+    public static void print(int num) {
+    }
+
+    public static void print(String text) {
+    }
+
+    public static void print(char symbol) {
+    }
+
+    public static void print(boolean flag) {
+    }
+}
