a6ec9af9a49d292caeb32982d9e925e30f7fe911
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 14:47:07 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task12.task1206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static void print(int num) {
+    }
+
+    public static void print(String text) {
+    }
+}
