784bb08a33f1e62be5c0b4f6b8abaa3cd30a8910
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 23:26:37 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        Scanner console = new Scanner(System.in);
+        String name = console.nextLine();
+        int age = console.nextInt();
+        console.close();
+        if (age > 17 && age < 29) {
+            System.out.println(name + militaryCommissar);
+        }
+    }
+}
