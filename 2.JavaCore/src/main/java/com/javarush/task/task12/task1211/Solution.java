b99ae4de2ab6975b827336267b19025d934473dd
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 16:36:33 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task12/task1211/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task12.task1211;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static abstract class Pet {
+
+        public abstract String getName();
+    }
+}
