6091d35f682e9d976a88b6c5ca1a1c28597ae158
JavaNickUli <java.nickuli@mail.ru>
2022-11-07 18:28:30 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0212/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0212;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+        System.out.println(usa.toUpperCase());
+        System.out.println("Винни Пух".toUpperCase());
+        System.out.println((caps + "they know I mean business").toUpperCase());
+    }
+}
