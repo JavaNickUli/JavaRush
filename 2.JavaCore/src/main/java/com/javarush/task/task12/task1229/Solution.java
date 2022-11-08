5e2287d87efe01d9238d40cd888cad9bf969172c
JavaNickUli <java.nickuli@mail.ru>
2022-11-08 17:56:59 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task12/task1229/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1229;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+
+        public void workHard();
+    }
+
+    public static class Director {
+
+        public void workHard() {
+        }
+    }
+
+    public static class CTO extends Director implements Businessman {
+    }
+}
