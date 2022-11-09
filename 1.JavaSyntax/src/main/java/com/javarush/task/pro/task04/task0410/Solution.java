b659db0389e2fea838ca5ec64a932b965297117c
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 00:10:31 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0410/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task04.task0410;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        int secondMin = min;
+        while (scanner.hasNextInt()) {
+            int temp = scanner.nextInt();
+            if (min > temp) {
+                secondMin = min;
+                min = temp;
+            }
+            if (secondMin > temp && min != temp) {
+                secondMin = temp;
+            }
+        }
+        System.out.println(secondMin);
+    }
+}
