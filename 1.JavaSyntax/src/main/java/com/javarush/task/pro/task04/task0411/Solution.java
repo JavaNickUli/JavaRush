07bb25333751ff103151c562a2ee98c599406720
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 00:15:44 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0411/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task04.task0411;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 16; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
