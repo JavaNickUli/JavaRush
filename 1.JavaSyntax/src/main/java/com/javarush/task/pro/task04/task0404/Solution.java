913a67e2eb9ca4b0603169b4236e7c63ee1f1b41
JavaNickUli <java.nickuli@mail.ru>
2022-11-09 23:48:08 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task04.task0404;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int height = 5;
+        while (height-- > 0) {
+            int width = 10;
+            while (width-- > 0) {
+                System.out.print("Q");
+            }
+            System.out.println();
+        }
+    }
+}
