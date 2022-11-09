4bcef45df302b05ca3410a60530cbe792fe15002
JavaNickUli <java.nickuli@mail.ru>
2022-11-09 23:52:15 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0405;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int height = 10;
+        while (height-- > 0) {
+            int width = 20;
+            while (width-- > 0) {
+                boolean symbol = (height == 0 || height == 9 ||
+                        width == 0 || width == 19);
+                System.out.print(symbol ? "Б" : " ");
+            }
+            System.out.println();
+        }
+    }
+}
