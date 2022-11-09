9ccab86953f2ff2e5b25e29d5b1638419b8fa757
JavaNickUli <java.nickuli@mail.ru>
2022-11-09 15:12:31 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task13/task1313/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task13.task1313;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+
+        String getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
