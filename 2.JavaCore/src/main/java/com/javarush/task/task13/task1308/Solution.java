fa06dab84922dcb81d762482d0d57bca31406d07
JavaNickUli <java.nickuli@mail.ru>
2022-11-09 13:36:50 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task13/task1308/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task13.task1308;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Person {
+
+        boolean isAlive();
+    }
+
+    public interface Presentable extends Person {
+    }
+}
