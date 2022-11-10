63ab5a0491f10dbb0052976a499e65b3dc3797e7
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 12:20:16 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task13/task1322/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task13.task1322;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+
+        SimpleObject<T> getInstance();
+    }
+
+    public static class StringObject implements SimpleObject<String> {
+
+        @Override
+        public SimpleObject<String> getInstance() {
+            return this;
+        }
+    }
+}
