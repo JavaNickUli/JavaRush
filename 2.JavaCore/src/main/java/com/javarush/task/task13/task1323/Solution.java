0507300604b9693b2e423e648cb86891485d3d1e
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 12:28:36 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task13/task1323/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task13.task1323;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+
+        void refresh();
+    }
+
+    class Screen implements Updatable {
+
+        @Override
+        public void onSelect() {
+        }
+
+        @Override
+        public void refresh() {
+        }
+    }
+}
