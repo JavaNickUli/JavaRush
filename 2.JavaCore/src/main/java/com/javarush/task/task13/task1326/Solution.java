b790d7e503ac25d9e6f8c100256f247b86902258
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 14:06:17 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task13/task1326/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task13.task1326;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(new Scanner(System.in).nextLine());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        List<Integer> list = new ArrayList<>();
+        while (bufferedReader.ready()) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+            if (num % 2 == 0) {
+                list.add(num);
+            }
+        }
+        Collections.sort(list);
+        list.forEach(System.out::println);
+        fileInputStream.close();
+        bufferedReader.close();
+    }
+}
