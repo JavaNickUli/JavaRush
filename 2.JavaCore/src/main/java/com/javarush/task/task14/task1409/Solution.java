ae032c3c0c66f35dd471ce9eeb323f58d14340e7
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 18:17:21 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task14/task1409/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task14.task1409;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        println(new WaterBridge());
+        println(new SuspensionBridge());
+    }
+
+    public static void println(Bridge bridge) {
+        System.out.println(bridge.getCarsCount());
+    }
+}
