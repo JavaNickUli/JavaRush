2424241f731e419dfe1109808b7d82ffb12029ce
JavaNickUli <java.nickuli@mail.ru>
2022-11-10 22:33:40 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task05/task0501/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task05.task0501;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
