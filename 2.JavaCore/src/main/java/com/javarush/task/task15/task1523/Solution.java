fc3ba376344f11197dd80bea91e01fe948fefa3c
JavaNickUli <java.nickuli@mail.ru>
2022-11-14 21:34:15 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task15.task1523;
+
+public class Solution {
+
+    private Solution(byte aByte) {
+    }
+
+    protected Solution(short aShort) {
+    }
+
+    Solution(int anInt) {
+    }
+
+    public Solution(long aLong) {
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ 2.JavaCore/src/main/java/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    protected SubSolution(short aShort) {
+        super(aShort);
+    }
+
+    SubSolution(int anInt) {
+        super(anInt);
+    }
+
+    public SubSolution(long aLong) {
+        super(aLong);
+    }
+}
