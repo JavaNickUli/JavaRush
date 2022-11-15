bb10b59662c4fd620d86868ab43f8e291b3efe38
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 22:40:07 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task06.task0606;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static void universalMethod() {
+    }
+
+    public static void universalMethod(int age) {
+    }
+
+    public static void universalMethod(Long bigNumber) {
+    }
+
+    public static void universalMethod(boolean flag) {
+    }
+
+    public static void universalMethod(String text) {
+    }
+
+    public static void universalMethod(String text, int number) {
+    }
+
+    public static void universalMethod(double width, double height) {
+    }
+
+    public static void universalMethod(double weight) {
+    }
+
+    public static void universalMethod(String name, String surname) {
+    }
+
+    public static void universalMethod(int[] array) {
+    }
+}
+
