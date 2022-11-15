61dbe1ba1f4dda76a896d5eaaf9450b4f4381de1
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 16:09:19 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task16/task1615/Solution.java
@@ -0,0 +1,76 @@
+package com.javarush.task.task16.task1615;
+
+public class Solution {
+
+    public static volatile Runway RUNWAY = new Runway();
+
+    public static void main(String[] args) throws InterruptedException {
+        Plane plane1 = new Plane("Самолет #1");
+        Plane plane2 = new Plane("Самолет #2");
+        Plane plane3 = new Plane("Самолет #3");
+    }
+
+    private static void waiting() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+
+    }
+
+    private static void takingOff() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public static class Plane extends Thread {
+
+        public Plane(String name) {
+            super(name);
+            start();
+        }
+
+        public void run() {
+            boolean isAlreadyTakenOff = false;
+            while (!isAlreadyTakenOff) {
+                if (RUNWAY.trySetTakingOffPlane(this)) {
+                    System.out.println(getName() + " взлетает");
+                    takingOff();// взлетает
+                    System.out.println(getName() + " уже в небе");
+                    isAlreadyTakenOff = true;
+                    RUNWAY.setTakingOffPlane(null);
+                } else if (!this.equals(RUNWAY.getTakingOffPlane())) {
+                    System.out.println(getName() + " ожидает");
+                    waiting();
+                }
+            }
+        }
+    }
+
+    public static class Runway {
+
+        private Thread t;
+
+        public Thread getTakingOffPlane() {
+            return t;
+        }
+
+        public void setTakingOffPlane(Thread t) {
+            synchronized (this) {
+                this.t = t;
+            }
+        }
+
+        public boolean trySetTakingOffPlane(Thread t) {
+            synchronized (this) {
+                if (this.t == null) {
+                    this.t = t;
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
