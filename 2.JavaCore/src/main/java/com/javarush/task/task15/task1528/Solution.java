21f8fc580713655e30e254c2e80fc24a2a6518de
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 08:22:36 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task15/task1528/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1528;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Hryvnia().getAmount());
+    }
+
+    public static abstract class Money {
+
+        abstract Money getMoney();
+
+        public Object getAmount() {
+            return getMoney().getAmount();
+        }
+    }
+
+    public static class Hryvnia extends Money {
+
+        private double amount = 123d;
+
+        public Hryvnia getMoney() {
+            return this;
+        }
+
+        @Override
+        public Object getAmount() {
+            return this.amount;
+        }
+    }
+}
