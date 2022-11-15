bf7c78673d462039d35166836556000789e0a368
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 22:47:02 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task06.task0608;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static long cube(long number) {
+        return number * number * number;
+    }
+}
