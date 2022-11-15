a3fcae904bb04f56a0fbde6149be67d8010794f7
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 16:44:39 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task16.task1617;
+
+public class Solution {
+
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+                while (numSeconds > 0) {
+                    System.out.print(numSeconds-- + " ");
+                    Thread.sleep(1000);
+                }
+                System.out.println("Марш!");
+            } catch (InterruptedException e) {
+                System.out.println("Прервано!");
+            }
+        }
+    }
+}
