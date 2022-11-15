861b83a336b4769fc70d0a081992567347a2efa1
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 22:36:17 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task06.task0605;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[array.length - i - 1];
+            array[array.length - i - 1] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
