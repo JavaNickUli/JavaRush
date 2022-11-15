77a32fa95f353ce3356d6a6b5f6339657e585bb5
JavaNickUli <java.nickuli@mail.ru>
2022-11-15 16:28:25 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task16/task1616/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task16.task1616;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(in);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.start();
+        reader.readLine();
+        stopwatch.interrupt();
+        reader.close();
+        in.close();
+    }
+
+    public static class Stopwatch extends Thread {
+
+        private int seconds;
+
+        public void run() {
+            try {
+                while (!this.isInterrupted()) {
+                    Thread.sleep(1000);
+                    seconds++;
+                }
+            } catch (InterruptedException e) {
+                System.out.println(seconds);
+            }
+        }
+    }
+}
