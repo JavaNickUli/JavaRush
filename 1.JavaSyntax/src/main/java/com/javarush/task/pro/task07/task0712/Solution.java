3f37d29c8995bcfec80a2fc50d42d8519325c910
JavaNickUli <java.nickuli@mail.ru>
2022-11-16 17:43:08 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task07.task0712;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String text0 = new String();
+        String text1 = new String();
+        String text2 = new String();
+        String text3 = new String();
+        String text4 = new String();
+        String text5 = new String();
+        String text6 = new String();
+        String text7 = new String();
+        String text8 = new String();
+        String text9 = new String();
+        int[] array0 = new int[1];
+        int[] array1 = new int[2];
+        int[] array2 = new int[3];
+        int[] array3 = new int[4];
+        int[] array4 = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
+    }
+}
