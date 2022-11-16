812d8d579eb31b49251af79a4e13399c0bc2b21e
JavaNickUli <java.nickuli@mail.ru>
2022-11-16 17:14:29 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0705/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0705;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        div(0, 1);
+        div(0, -1);
+    }
+
+    public static void div(double a, double b) {
+        System.out.println(b / a);
+    }
+}
