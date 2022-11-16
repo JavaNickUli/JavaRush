e5c52c1ff46893609eee97c4051ee2ef9f1e2f46
JavaNickUli <java.nickuli@mail.ru>
2022-11-16 17:29:34 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0709/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task07.task0709;
+
+public class Solution {
+
+    long a = 109 + 15;
+    int b = (int) (a * 2);
+    short c = (short) (a / b);
+    byte d = (byte) (a + b - c);
+}
