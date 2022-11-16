067c96443b5e8d61572a5884e4ca27d002346499
JavaNickUli <java.nickuli@mail.ru>
2022-11-16 16:58:29 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0701;
+
+public class Solution {
+
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    char aChar;
+    boolean aBoolean;
+}
