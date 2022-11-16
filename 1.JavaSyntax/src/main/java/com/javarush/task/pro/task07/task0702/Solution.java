c329fc7f05cbae6863f535fb84816bf84e29951c
JavaNickUli <java.nickuli@mail.ru>
2022-11-16 17:02:16 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task07.task0702;
+
+public class Solution {
+
+    byte aByte = Byte.MAX_VALUE;
+    short aShort = Short.MAX_VALUE;
+    int anInt = Integer.MAX_VALUE;
+    long aLong = Long.MAX_VALUE;
+}
