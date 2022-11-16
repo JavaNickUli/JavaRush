4aacef43417f7b20bf58f6382cce3d3d54631a15
JavaNickUli <java.nickuli@mail.ru>
2022-11-16 22:52:13 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0714/Car.java
@@ -0,0 +1,7 @@
+package com.javarush.task.pro.task07.task0714;
+
+public class Car extends Vehicle {
+
+    int wheelCount;
+    double weight;
+}
---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task07/task0714/Vehicle.java
@@ -0,0 +1,6 @@
+package com.javarush.task.pro.task07.task0714;
+
+public class Vehicle {
+
+    double maxSpeed;
+}
