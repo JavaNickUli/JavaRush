7bad1443263363802cd329e00f52aa5037db8037
JavaNickUli <java.nickuli@mail.ru>
2022-11-17 11:19:25 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task16.task1619;
+
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        TestThread.run = false;
+    }
+
+    public static class TestThread implements Runnable {
+
+        static boolean run = true;
+
+        public void run() {
+            while (run) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
