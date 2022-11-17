175fcdd0d538f409abbaeaba6d049608d779570e
JavaNickUli <java.nickuli@mail.ru>
2022-11-17 16:02:00 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task16/task1623/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task16.task1623;
+
+public class Solution {
+
+    static int count = 15;
+    static volatile int createdThreadCount;
+
+    public static void main(String[] args) {
+        System.out.println(new GenerateThread());
+    }
+
+    public static class GenerateThread extends Thread {
+
+        public GenerateThread() {
+            super(String.valueOf(++createdThreadCount));
+            start();
+        }
+
+        @Override
+        public String toString() {
+            return getName() + " created";
+        }
+
+        @Override
+        public void run() {
+            if (createdThreadCount < count) {
+                System.out.println(new GenerateThread());
+            }
+        }
+    }
+}
