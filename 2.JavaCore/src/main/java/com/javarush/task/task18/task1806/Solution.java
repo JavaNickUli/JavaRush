b04ffecbae6f97fee87ee89d860e5c6572434f51
JavaNickUli <java.nickuli@mail.ru>
2022-11-23 11:15:55 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task18/task1806/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task18.task1806;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("c:/data.txt");
+        FileOutputStream outputStream = new FileOutputStream("c:/result.txt");
+        if (inputStream.available() > 0) {
+            byte[] buffer = new byte[65535];
+            int count = inputStream.read(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+}
