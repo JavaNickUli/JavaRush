60ec6d22a182f7efd1f519d8b0131cdeb7450c05
JavaNickUli <java.nickuli@mail.ru>
2022-11-23 13:50:37 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task18.task1810;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws DownloadException {
+        BufferedReader pathReader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try (FileInputStream fileInputStream = new FileInputStream(pathReader.readLine())) {
+                if (fileInputStream.getChannel().size() < 1000) {
+                    throw new DownloadException();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public static class DownloadException extends Exception {
+    }
+}
