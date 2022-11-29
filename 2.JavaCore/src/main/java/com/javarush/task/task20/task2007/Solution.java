3c0dec60152d165c553941d619425a9fb2dee4d6
JavaNickUli <java.nickuli@mail.ru>
2022-11-29 21:48:57 +0300

    Solution

---
+++ 2.JavaCore/src/main/java/com/javarush/task/task20/task2007/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task20.task2007;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static class JavaRush implements Serializable {
+        public List<User> users = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+    }
+}
