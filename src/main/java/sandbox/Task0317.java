608efc049688ad482ad2b7e26387bfce57363687
JavaNickUli <java.nickuli@mail.ru>
2022-12-02 00:12:10 +0300

    Solution

--- src/main/java/sandbox/Task0317.java
+++ src/main/java/sandbox/Task0317.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Task0317 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number;
-        for (int i = 0; i < 4; i++) {
-            if ((number = scanner.nextInt()) > 0) {
-                System.out.println(number);
-            }
-        }
+        for (int i = 4, n; i > 0; System.out.print((n = new Scanner(System.in).nextInt()) > 0 ? n + "\n" : ""), i--);
+//        Scanner scanner = new Scanner(System.in);
+//        for (int i = 0, number; i < 4; i++) {
+//            if ((number = scanner.nextInt()) > 0) {
+//                System.out.println(number);
+//            }
+//        }
     }
 }
