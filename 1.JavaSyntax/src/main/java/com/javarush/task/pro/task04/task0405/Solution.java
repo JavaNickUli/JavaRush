6b9e11f1236bd606a8057ac9a34160b48c801e03
JavaNickUli <java.nickuli@mail.ru>
2022-12-12 00:01:34 +0300

    Solution

--- 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0405/Solution.java
@@ -3,12 +3,11 @@ package com.javarush.task.pro.task04.task0405;
 public class Solution {
 
     public static void main(String[] args) {
-        int height = 10;
-        while (height-- > 0) {
-            int width = 20;
-            while (width-- > 0) {
-                boolean symbol = (height == 0 || height == 9 || width == 0 || width == 19);
-                System.out.print(symbol ? "Б" : " ");
+        int height = 10, width = 20;
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                boolean isEdge = i == 1 || i == height || j == 1 || j == width;
+                System.out.print(isEdge ? "Б" : " ");
             }
             System.out.println();
         }
