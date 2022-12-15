ba867493730e0356c6e08a05353dfed3cf7fa849
JavaNickUli <java.nickuli@mail.ru>
2022-12-15 20:35:09 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task01/task0101/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task01.task0101;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Диего + Амиго = Друзья");
+    }
+}
