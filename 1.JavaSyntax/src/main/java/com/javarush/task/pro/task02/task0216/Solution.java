8055a87ae28ca93d263b7eb57f14bf3589c52df3
JavaNickUli <java.nickuli@mail.ru>
2022-12-15 23:47:25 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0216;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String name = "Учёный";
+        String date = "вчера";
+        String time = "ночью";
+        String adjective = "синтетический";
+        String noun = "накопитель";
+        String verb = "сохраняет";
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb +
+                " террабайты данных в минуту.");
+    }
+}
