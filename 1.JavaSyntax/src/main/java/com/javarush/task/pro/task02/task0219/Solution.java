b75b68bfd02edd67b19712c0d7d79076905bb12c
JavaNickUli <java.nickuli@mail.ru>
2022-12-15 22:26:48 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0219;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int id = 13;
+        int age = 22;
+        String documents = "passport";
+        String behavior = "dissolute";
+    }
+}
