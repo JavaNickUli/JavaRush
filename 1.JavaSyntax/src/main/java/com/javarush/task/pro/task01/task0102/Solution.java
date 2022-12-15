85a9a6d6c2bce8541e31b8bd797372047174f14e
JavaNickUli <java.nickuli@mail.ru>
2022-12-15 20:47:37 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task01/task0102/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task01.task0102;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Аварийное питание включено");
+        System.out.println("Ручное управление включено");
+    }
+}
