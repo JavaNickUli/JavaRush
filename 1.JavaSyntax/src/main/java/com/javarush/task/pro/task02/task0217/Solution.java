ae6af9ee348d09440a01011efd93d3cfd09b3319
JavaNickUli <java.nickuli@mail.ru>
2022-12-16 19:10:06 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0217/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0217;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int numberOne = 3;
+        int numberTwo = 1;
+        int numberThree = 1;
+        String string = "00";
+
+        String result = numberTwo + string + (numberOne + numberTwo + numberThree) + string;
+        System.out.println("Разгоняем Амиго на " + result + "%");
+    }
+}
