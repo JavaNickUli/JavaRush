cabe73bde355c24848b901da7950f4711822d66b
JavaNickUli <java.nickuli@mail.ru>
2022-12-16 18:52:08 +0300

    Solution

--- 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0213/Solution.java
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0213/Solution.java
@@ -6,6 +6,7 @@ public class Solution {
         String title = "Senior Lead Principal Software Engineer Data Architect";
         String degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
         String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
+
         System.out.println("RESUME".toLowerCase());
         System.out.println(("TITLE: " + title).toLowerCase());
         System.out.println(("DEGREE: " + degree).toLowerCase());
