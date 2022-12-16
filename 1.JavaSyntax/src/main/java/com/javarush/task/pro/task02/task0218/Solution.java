9564c2295d3410e6b3858a776aaad3349555b675
JavaNickUli <java.nickuli@mail.ru>
2022-12-16 19:57:05 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        System.out.println(num1 * num2 * num3);
+    }
+}
