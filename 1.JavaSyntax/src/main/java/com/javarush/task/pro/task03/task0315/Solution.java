ab10b1c2117a31bbc938f9b4626360df6879afc1
JavaNickUli <java.nickuli@mail.ru>
2022-12-16 21:35:13 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task03/task0315/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task03.task0315;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        boolean isFabricOpen = true;
+        if (isFabricOpen) {
+            System.out.println("Дверь открыта");
+        } else {
+            System.out.println("Дверь закрыта");
+        }
+    }
+}
