52a70cecf94dc1ae5dbea83dc51583d1bded3ca1
JavaNickUli <java.nickuli@mail.ru>
2022-12-20 16:30:25 +0300

    Solution

--- Games/src/main/java/com/javarush/games/minesweeper/MinesweeperGame.java
+++ Games/src/main/java/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,9 +5,20 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
 
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField.length; j++) {
+                gameField[i][j] = new GameObject(i, j);
+                setCellColor(i, j, Color.ORANGE);
+            }
+        }
     }
 }
