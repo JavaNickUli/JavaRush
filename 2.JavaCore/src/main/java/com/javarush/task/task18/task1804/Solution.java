89c2001ce0ba3f0541d379189724d378992332d3
JavaNickUli <java.nickuli@mail.ru>
2022-12-20 21:28:21 +0300

    Solution

--- 2.JavaCore/src/main/java/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/main/java/com/javarush/task/task18/task1804/Solution.java
@@ -12,16 +12,15 @@ public class Solution {
         reader.close();
         int[] bytes = new int[256];
         while (inputStream.available() > 0) {
-            int aByte = inputStream.read();
-            bytes[aByte] += 1;
-        }
-        int minCount = Integer.MAX_VALUE;
-        for (int quantity : bytes) {
-            if (quantity != 0 && minCount > quantity) {
-                minCount = quantity;
-            }
+            bytes[inputStream.read()] += 1;
         }
         inputStream.close();
+        int minCount = Integer.MAX_VALUE;
+        for (int frequency : bytes) {
+            if (frequency != 0 && minCount > frequency) {
+                minCount = frequency;
+            }
+        }
         for (int i = 0; i < bytes.length; i++) {
             if (bytes[i] == minCount) {
                 System.out.print(i + " ");
