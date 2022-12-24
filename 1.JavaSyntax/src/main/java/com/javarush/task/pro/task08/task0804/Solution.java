bc6212781105d280a725a4502428fcc4a529025d
JavaNickUli <java.nickuli@mail.ru>
2022-12-24 21:13:29 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task08/task0804/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task08.task0804;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(generateNumber());
+    }
+
+    public static int generateNumber() {
+        return (int) (Math.random() * 100);
+    }
+}
