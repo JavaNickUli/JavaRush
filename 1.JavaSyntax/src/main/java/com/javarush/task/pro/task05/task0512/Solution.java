5b2af8fa35834f3eb3f1571e107d4bf785d5191e
JavaNickUli <java.nickuli@mail.ru>
2022-12-24 11:09:02 +0300

    Solution

--- 1.JavaSyntax/src/main/java/com/javarush/task/pro/task05/task0512/Solution.java
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task05/task0512/Solution.java
@@ -5,10 +5,10 @@ public class Solution {
     public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
 
     public static void main(String[] args) {
-        for (int[][] ints : multiArray) {
-            for (int[] anInt : ints) {
-                for (int i : anInt) {
-                    System.out.print(i + " ");
+        for (int[][] matrix : multiArray) {
+            for (int[] ints : matrix) {
+                for (int el : ints) {
+                    System.out.print(el + " ");
                 }
             }
         }
