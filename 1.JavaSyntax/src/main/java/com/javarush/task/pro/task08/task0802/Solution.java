371b53c36bdf6596373f0b7159dd19da6769bcec
JavaNickUli <java.nickuli@mail.ru>
2022-12-24 19:05:05 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task08/task0802/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task08.task0802;
+
+public class Solution {
+
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double cbrt(double a) {
+        return Math.cbrt(a);
+    }
+
+    public static double pow(int number, int power) {
+        return Math.pow(number, power);
+    }
+}
