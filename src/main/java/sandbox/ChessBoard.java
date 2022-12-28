51b1ac5fad9b92e16ed3c11e4bf9b30710ba56f1
JavaNickUli <java.nickuli@mail.ru>
2022-12-28 10:11:15 +0300

    Solution

--- src/main/java/sandbox/ChessBoard.java
+++ src/main/java/sandbox/ChessBoard.java
@@ -6,7 +6,7 @@ public class ChessBoard {
         String[] color = {"\033[30;47m", "\033[37;40m"};
         for (int i = 8; i > 0; i--) {
             for (int j = 0; j < 8; j++) {
-                System.out.print(color[(i + j) % 2] + " " + (char) (65 + j) + i);
+                System.out.print(color[(i + j) % 2] + " " + (char) ('A' + j) + i);
             }
             System.out.println("\033[0m");
         }
