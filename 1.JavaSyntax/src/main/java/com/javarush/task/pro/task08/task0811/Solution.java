d5d96e4568b179841b92ba1e13c0cbe1a880be4c
JavaNickUli <java.nickuli@mail.ru>
2022-12-28 10:28:23 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task08/task0811/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task08.task0811;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int x = 7;
+//        x = x & 25;
+        x = x & 5;
+//        x = x & 3;
+        x = x & 12;
+        x = x | 1;
+
+        System.out.println(x);
+    }
+}
