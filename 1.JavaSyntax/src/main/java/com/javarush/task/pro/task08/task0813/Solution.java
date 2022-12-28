9772aed105ccf1fd6b5fb90fed982b6bf3c126ae
JavaNickUli <java.nickuli@mail.ru>
2022-12-28 22:59:08 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task08/task0813/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task08.task0813;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getPowerOfTwo(3));
+    }
+
+    public static int getPowerOfTwo(int power) {
+        return 1 << power;
+    }
+}
