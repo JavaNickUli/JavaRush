76929d4fd3ff62909e1290341b95048bc0b39e3a
JavaNickUli <java.nickuli@mail.ru>
2022-12-28 23:02:28 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task08/task0814/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task08.task0814;
+
+public class Solution {
+
+    public static int setFlag(int number, int flagPos) {
+        return number | 1 << flagPos;
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        return number & ~(1 << flagPos);
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        return (number & (1 << flagPos)) == (1 << flagPos);
+    }
+}
