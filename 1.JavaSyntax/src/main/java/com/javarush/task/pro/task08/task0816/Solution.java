a557f49786cb638c4f841cdd4f1a3ac9b21f43f3
JavaNickUli <java.nickuli@mail.ru>
2022-12-28 23:42:30 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task08/task0816/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task08.task0816;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println(number + number * (number + ++number));
+    }
+}
