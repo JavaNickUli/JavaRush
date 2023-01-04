75a048330b59746f4ddf14e25d4ec18701f2ad7c
JavaNickUli <java.nickuli@mail.ru>
2023-01-04 21:49:23 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1024/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task10.task1024;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new TerribleTwoBit().getMission());
+    }
+}
