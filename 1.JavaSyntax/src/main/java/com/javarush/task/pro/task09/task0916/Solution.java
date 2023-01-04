026528d9e65da78cd455a7fd33f049d451e47bd0
JavaNickUli <java.nickuli@mail.ru>
2023-01-04 16:52:38 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task09/task0916/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task09.task0916;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(format("Амиго", 5000));
+    }
+
+    public static String format(String name, int salary) {
+        String phrase = "Меня зовут %s. Я буду зарабатывать $%d в месяц.";
+        return String.format(phrase, name, salary);
+    }
+}
