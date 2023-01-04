51786118011ad6443aaf6ceb4a8c4926d47ea5a2
JavaNickUli <java.nickuli@mail.ru>
2023-01-04 21:32:19 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task10.task1006;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        GasCar gasCar = new GasCar();
+        HybridCar hybridCar = new HybridCar();
+    }
+}
