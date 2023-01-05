9e13758d16c7458a80e909fe7835a0f81d5a6c30
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 12:02:38 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task10.task1017;
+
+public class Earth {
+
+    public static void main(String[] args) {
+        Eurasia eurasia = new Eurasia(54);
+        Africa africa = new Africa(30);
+        NorthAmerica northAmerica = new NorthAmerica(24);
+        SouthAmerica southAmerica = new SouthAmerica(18);
+        Antarctica antarctica = new Antarctica(14);
+        Australia australia = new Australia(8);
+    }
+}
