d5164e2c0d3d16597446c6e728592f55c3f334c1
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 11:43:52 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1015/Hedgehog.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1015;
+
+public class Hedgehog {
+
+    public void eat(Apple apple) {
+        System.out.println("Яблоко было съедено!");
+    }
+
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        Hedgehog hedgehog = new Hedgehog();
+        hedgehog.eat(apple);
+    }
+
+    public static class Apple {
+    }
+}
