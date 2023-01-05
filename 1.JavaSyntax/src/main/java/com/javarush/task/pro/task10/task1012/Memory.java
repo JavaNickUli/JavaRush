a2329ac03cd1cde281b81e183c5ec27f45bc0d94
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 11:29:02 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1012/Memory.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task10.task1012;
+
+import java.util.Arrays;
+
+public class Memory {
+
+    public static void main(String[] args) {
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        executeDefragmentation(memory);
+        System.out.println(Arrays.toString(memory));
+    }
+
+    public static void executeDefragmentation(String[] array) {
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                array[j++] = array[i];
+                if (i > j) {
+                    array[i] = null;
+                }
+            }
+        }
+    }
+}
