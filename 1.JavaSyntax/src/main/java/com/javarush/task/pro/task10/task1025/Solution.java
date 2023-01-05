6756773a52a057d468c62d0d53c7337bf621b62a
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 09:38:20 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1025/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1025;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Repeater repeater = new Repeater();
+        if (repeater.getFrequency() < 100) {
+            repeater.setFrequency(repeater.getFrequency() * 10);
+        }
+        if (!"elliptic".equals(repeater.getTrajectory())) {
+            repeater.setTrajectory("elliptic");
+        }
+    }
+}
