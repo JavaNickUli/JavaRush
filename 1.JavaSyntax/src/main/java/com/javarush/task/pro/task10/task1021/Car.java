4b084b55540c13852795c764503aba959abc783c
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 12:31:15 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1021/Car.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1021;
+
+public class Car {
+
+    private String model;
+    private int year;
+    private String color;
+
+    public void initialize(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
