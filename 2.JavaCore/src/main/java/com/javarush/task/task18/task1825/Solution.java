9d3a87ea2e757f05bfbec384ba35787799dc97ce
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 14:47:51 +0300

    Solution

--- 2.JavaCore/src/main/java/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/main/java/com/javarush/task/task18/task1825/Solution.java
@@ -5,24 +5,24 @@ import java.io.*;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader pathReader = new BufferedReader(new InputStreamReader(System.in));
-        String path = null;
-        String part;
+        String filename = null;
         int maxNumberOfPart = 0;
-        while (!"end".equals(part = pathReader.readLine())) {
-            String[] temp = part.split(".part");
-            if (path == null) {
-                path = temp[0];
-            }
-            int numberOfPart = Integer.parseInt(temp[1]);
-            if (maxNumberOfPart < numberOfPart) {
-                maxNumberOfPart = numberOfPart;
+        try (BufferedReader pathReader = new BufferedReader(new InputStreamReader(System.in))) {
+            String path;
+            while (!"end".equals(path = pathReader.readLine())) {
+                String[] partsOfPath = path.split(".part");
+                if (filename == null) {
+                    filename = partsOfPath[0];
+                }
+                int numberOfPart = Integer.parseInt(partsOfPath[1]);
+                if (maxNumberOfPart < numberOfPart) {
+                    maxNumberOfPart = numberOfPart;
+                }
             }
         }
-        pathReader.close();
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path))) {
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename))) {
             for (int i = 1; i <= maxNumberOfPart; i++) {
-                String pathInput = path + ".part" + i;
+                String pathInput = filename + ".part" + i;
                 try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(pathInput))) {
                     while (input.available() > 0) {
                         out.write(input.read());
