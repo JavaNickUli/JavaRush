cd8b863aa8e279d1938b68cffedcb2eb697fc9d8
JavaNickUli <java.nickuli@mail.ru>
2023-01-05 09:25:35 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task10/task1007/Bugatti.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1007;
+
+public class Bugatti {
+
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
