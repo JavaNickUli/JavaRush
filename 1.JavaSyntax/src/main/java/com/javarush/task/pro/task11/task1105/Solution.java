4243d9590f1a5c76bb79053279c91998d0777ce9
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 12:06:06 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1105/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1105;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.printSize();
+        window.changeSize(4, 5);
+        window.printSize();
+    }
+}
