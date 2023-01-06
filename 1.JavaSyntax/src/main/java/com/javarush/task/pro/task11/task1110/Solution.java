15e52a22f25d5907db25e776fcea9873e50f8888
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 16:07:26 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1110/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task11.task1110;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
--- src/main/java/sandbox/Task2025.java
+++ src/main/java/sandbox/Task2025.java
@@ -1,43 +1,79 @@
 package sandbox;
 
 import java.util.Arrays;
-import java.util.Set;
 import java.util.TreeSet;
 
 public class Task2025 {
 
+    private static int depth;
+    private static int[] digits;
+    private static long maxValue;
+    private static long[][] pows;
+    private static TreeSet<Long> grandResult;
+
     public static long[] getNumbers(long N) {
-        int numberLength = String.valueOf(N).length();
-        long[][] powTable = new long[10][numberLength + 1];
-        for (int i = 0; i < powTable.length; i++) {
-            for (int j = 0; j < powTable[0].length; j++) {
-                powTable[i][j] = i;
-                for (int k = 1; k < j; k++) {
-                    powTable[i][j] *= i;
+        long[] result = null;
+        int maxDepth = (int) Math.ceil(Math.log10(N));       // maximum long depth = 19 on Long.MAX_VALUE
+        if (maxDepth > 0) {
+            maxValue = N;
+            digits = new int[maxDepth];
+            pows = new long[11][maxDepth];
+            grandResult = new TreeSet<>();
+            for (int i = 1; i < pows.length; i++) {
+                pows[i][0] = i;
+                for (int j = 1; j < pows[i].length; j++) {
+                    pows[i][j] = pows[i][j - 1] * i;
+                }
+            }
+            for (depth = 0; depth < maxDepth; ++depth) {
+                capacityIteration(0, depth);
+            }
+            int counter = 0;
+            result = new long[grandResult.size()];
+            for (long value : grandResult) {
+                result[counter++] = value;
+            }
+        }
+        return result;
+    }
+
+    public static void capacityIteration(int start, int capacity) {
+        for (start = start == 0 && capacity == 0 ? 1 : start; start <= 9; ++start) {
+            digits[capacity] = start;
+            if (capacity > 0) {
+                capacityIteration(start, capacity - 1);
+            } else {
+                long sumPows = 0;
+                for (int i = 0; i <= depth; ++i) {
+                    sumPows += pows[digits[i]][depth];
+                }
+                char[] chars = String.valueOf(sumPows).toCharArray();
+                if (chars.length - 1 == depth && sumPows < maxValue) {
+                    firstBlock:
+                    {
+                        for (int i = 0; i <= depth; ++i) {
+                            secondBlock:
+                            {
+                                for (int j = 0; j < chars.length; j++) {
+                                    if (digits[i] + 48 == chars[j]) {
+                                        chars[j] = 0;
+                                        break secondBlock;
+                                    }
+                                }
+
+                                break firstBlock;
+                            }
+                        }
+                        grandResult.add(sumPows);
+                    }
                 }
             }
         }
-        Set<Long> numbersOfArmstrong = new TreeSet<>();
-        long sum = 0;
-        int[] numsArray = new int[numberLength];
-        numsArray[numsArray.length - 1] = 1;
-        while (sum < N) {
-            sum = 0;
-            for (int num : numsArray) {
-                sum += powTable[num][numberLength];
-            }
-        }
-//        int[] sumOfSign = new int[numsArray.length];
-//        int delta = numsArray.length - sumLine.length();
-//        for (int j = 0; j < sumLine.length(); j++) {
-//            sumOfSign[j + delta] = sumLine.charAt(j) - '0';
-//        }
-        return null;
     }
 
     public static void main(String[] args) {
         long a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(getNumbers(1000)));
+        System.out.println(Arrays.toString(getNumbers(Long.MAX_VALUE)));
         long b = System.currentTimeMillis();
         System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
         System.out.println("time = " + (b - a) / 1000);
