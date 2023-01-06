2a7d6225e4553f0f73c996a3b70b774ffddeddaf
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 11:55:34 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task11.task1104;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String string = "12.84";
+        System.out.println(Math.round(Double.parseDouble(string)));
+    }
+}
