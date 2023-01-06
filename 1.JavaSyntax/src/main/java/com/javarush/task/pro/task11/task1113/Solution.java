2c23dd3ca1453a49e200ffa9e340f1198a0edad7
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 16:21:43 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1113/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task11.task1113;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
