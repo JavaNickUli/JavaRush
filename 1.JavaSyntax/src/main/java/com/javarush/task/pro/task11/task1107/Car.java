7fe0008ea8c27ed89b5944fb4a0ac9b1d84157e7
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 12:49:57 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task11.task1107;
+
+public class Car {
+
+    Engine engine;
+
+    class Engine {
+
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+        }
+
+        public void stop() {
+            isRunning = false;
+        }
+    }
+}
