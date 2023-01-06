354bd5e9b78b3efbaa75913b1695157ec8a63f99
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 12:58:21 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1109/Outer.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task11.task1109;
+
+public class Outer {
+
+    class Inner {
+        Inner() {
+            System.out.println("Создание объекта внутреннего класса");
+        }
+    }
+
+    static class Nested {
+        Nested() {
+            System.out.println("Создание объекта вложенного класса");
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1109/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task11.task1109;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Outer.Inner inner = new Outer().new Inner();
+        Outer.Nested nested = new Outer.Nested();
+    }
+}
