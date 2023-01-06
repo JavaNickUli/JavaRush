278a5e955bed449b313cf78494eef21ab99b91af
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 16:18:36 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1112/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task11.task1112;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
