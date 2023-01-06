02ad146251909a5dbb39499e3769c6bb6787f6b2
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 13:30:32 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1132/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task11.task1132;
+
+public class Solution {
+
+    public static RoboZombie[] roboZombies = Cosmoport.RoboZombieFactory.produce();
+
+    public static void main(String[] args) {
+        Cosmoport.UltraMetal.play();
+    }
+}
