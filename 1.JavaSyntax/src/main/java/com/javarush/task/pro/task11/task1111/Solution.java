061418faa8662e6bd04c88c544fe9dd2169bc4a0
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 16:14:09 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1111/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task11.task1111;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
