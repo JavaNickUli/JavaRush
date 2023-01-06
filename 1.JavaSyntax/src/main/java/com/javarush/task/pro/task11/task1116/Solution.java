b0696a138355cf9a1bab7642178192ec411c442d
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 16:35:35 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1116/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1116;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+}
+
+class Cat {
+    String name = "Tom";
+}
