35a3d6f433368ccc1c35ddbdf10888a3e411db9d
JavaNickUli <java.nickuli@mail.ru>
2023-01-06 11:47:11 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1130/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1130;
+
+public class Solution {
+
+    public static final String[] lyrics = {"Du...", "Du hast...", "Du hast ultra metal"};
+
+    public static void main(String[] args) {
+        UltraMetal ultraMetal = new UltraMetal();
+        ultraMetal.playUltraMetal(lyrics[0], lyrics[1], lyrics[2]);
+    }
+}
