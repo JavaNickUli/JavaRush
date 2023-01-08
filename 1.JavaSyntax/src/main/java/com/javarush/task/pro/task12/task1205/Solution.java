9f015e123366f458bf26364e1e2ec78b5b979757
JavaNickUli <java.nickuli@mail.ru>
2023-01-08 20:51:16 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task12/task1205/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task12.task1205;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divide(1, 0);
+        divide(-1, 0);
+        divide(0, 0);
+        divide(100, 20);
+        divide(200, 20);
+    }
+
+    public static void divide(double a, double b) {
+        System.out.println(a / b);
+    }
+}
