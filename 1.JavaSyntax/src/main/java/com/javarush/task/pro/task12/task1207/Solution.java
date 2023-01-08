42647167d0a1ed6fe2226d985d0d8c3325db3075
JavaNickUli <java.nickuli@mail.ru>
2023-01-08 22:08:50 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task12.task1207;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int first = 2;
+        int second = 1;
+        Boolean isLess = isLess(first, second);
+        if (isLess) {
+            System.out.println("Первое число меньше второго");
+        } else {
+            System.out.println("Первое число больше или равно второму числу");
+        }
+    }
+
+    public static Boolean isLess(int first, int second) {
+        return first < second;
+    }
+}
