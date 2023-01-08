a63781ca5c2459619422223f9875a9cf603752cf
JavaNickUli <java.nickuli@mail.ru>
2023-01-08 15:15:33 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task11/task1121/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1121;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
