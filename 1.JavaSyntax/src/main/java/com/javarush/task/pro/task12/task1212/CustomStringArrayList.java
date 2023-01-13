37b8ccebb72f45cc2b11ec75d319fd2fd3146494
JavaNickUli <java.nickuli@mail.ru>
2023-01-13 23:35:19 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task12/task1212/CustomStringArrayList.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task12.task1212;
+
+import java.util.Arrays;
+
+public class CustomStringArrayList {
+
+    private int size;
+    private int capacity;
+    private String[] elements;
+
+    public CustomStringArrayList() {
+        capacity = 10;
+        size = 0;
+        elements = new String[capacity];
+    }
+
+    public void add(String element) {
+        if (size == capacity) {
+            grow();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    private void grow() {
+        elements = Arrays.copyOf(elements, capacity *= 1.5);
+    }
+}
