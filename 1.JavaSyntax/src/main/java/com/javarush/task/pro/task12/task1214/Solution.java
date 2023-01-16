d973218eaf9ebf4614a37f100702be0faa0e9210
JavaNickUli <java.nickuli@mail.ru>
2023-01-16 16:21:20 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task12/task1214/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task12.task1214;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    public static ArrayList<String> programmingLanguages = new ArrayList<>(Arrays.asList(
+            "C", "C++", "Python", "JavaScript", "Ruby", "Java", "Pascal"));
+
+    public static void main(String[] args) {
+        programmingLanguages.remove("Pascal");
+    }
+}
