0347f9213eb9a4314c8720be7781c968e4dfc151
JavaNickUli <java.nickuli@mail.ru>
2023-01-22 11:56:26 +0300

    Solution

---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task12/task1218/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task12.task1218;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
---
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task12/task1219/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task12.task1219;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
