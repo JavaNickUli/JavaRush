7206d50c00daae31b96d09a535ad7bfd0f2c9ea6
JavaNickUli <java.nickuli@mail.ru>
2023-01-23 08:58:51 +0300

    Solution

---
+++ src/main/java/sandbox/jdk13/Task0706a.java
@@ -0,0 +1,25 @@
+package sandbox.jdk13;
+
+import java.util.Scanner;
+
+public class Task0706a {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] count = new int[15];
+        for (int i = 0; i < count.length; i++) {
+            count[i] = in.nextInt();
+        }
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (i % 2 == 0) {
+                even += count[i];
+            } else {
+                odd += count[i];
+            }
+        }
+        String particle = even - odd > 0 ? "" : "не";
+        System.out.println("В домах с " + particle + "четными номерами проживает больше жителей.");
+    }
+}
