d8a2cda3429fc2bcc3c0a3259353d55c5ac24f0e
JavaNickUli <java.nickuli@mail.ru>
2023-01-26 19:29:35 +0300

    Solution

--- 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/main/java/com/javarush/task/pro/task04/task0405/Solution.java
@@ -4,8 +4,10 @@ public class Solution {
 
     public static void main(String[] args) {
         int height = 10, width = 20;
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= width; j++) {
+        int i = 0;
+        while (i++ < height) {
+            int j = 0;
+            while (j++ < width) {
                 boolean isEdge = i == 1 || i == height || j == 1 || j == width;
                 System.out.print(isEdge ? "Б" : " ");
             }
